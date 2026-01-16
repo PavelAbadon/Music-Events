6b57886374eefc2c987675c65feb797509cc8be8
Pavel Vasilev <pavelabadon@gmail.com>
2026-01-16 11:59:09 +0200

    make controled form in login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 export default function Login() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const onChange = (e) => {
+        setFormData(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+
+
   return (
     <div className="auth-page">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={onSubmit}>
         <h2>Login</h2>
 
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <input
             type="email"
+            name="email"
+            value={formData.email}
             id="email"
             placeholder="metalhead@example.com"
+            onChange={onChange}
           />
         </div>
 
@@ -17,8 +38,11 @@ export default function Login() {
           <label htmlFor="password">Password</label>
           <input
             type="password"
+            name="password"
+            value={formData.password}
             id="password"
             placeholder="••••••••"
+            onChange={onChange}
           />
         </div>
 
